6a3271f61e3847bf87dcf028cacf5d86ad7755ce
David Betancourt <dmarquez.develop@gmail.com>
2025-07-31 01:28:54 +0000

    home page used to create new contact, added

--- src/pages/Demo.jsx
+++ src/pages/Demo.jsx
@@ -34,6 +34,27 @@ export const Demo = () => {
         })}
       </ul>
       <br />
+      <div className="card mb-3" style={{maxWidth: '540px'}}>
+				<div className="row g-0">
+					<div className="col-md-4">
+						<img src="https://picsum.photos/200/300" className="img-fluid rounded-start" alt="..."/>
+					</div>
+					<div className="col-md-8">
+						<div className="card-body">
+							<h5 className="card-title">Card title</h5>
+							<p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+							<p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              <button className="btn btn-success" 
+                onClick={() => dispatch({
+                  type: "add_task", 
+                  payload: { id: item.id, color: '#ffa500' }
+                })}>
+                Change Color
+              </button>
+						</div>
+					</div>
+				</div>
+			</div>
 
       <Link to="/">
         <button className="btn btn-primary">Back home</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,30 @@ import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container mt-5">
+			<div className="row">
+				<div className="mb-3">
+					<label for="formGroupExampleInput" className="form-label">Full Name</label>
+					<input type="text" className="form-control" id="formGroupExampleInput" placeholder="Full Name" />
+				</div>
+				<div className="mb-3">
+					<label for="formGroupExampleInput2" className="form-label">Email</label>
+					<input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Enter email" />
+				</div>
+				<div className="mb-3">
+					<label for="formGroupExampleInput" className="form-label">Phone</label>
+					<input type="text" className="form-control" id="formGroupExampleInput" placeholder="Enter phone" />
+				</div>
+				<div className="mb-3">
+					<label for="formGroupExampleInput2" className="form-label">Address</label>
+					<input type="text" className="form-control" id="formGroupExampleInput2" placeholder="Enter address" />
+				</div>
+				<button className="col rounded btn btn-primary"  type="button">Save</button>
+			</div>
+			
 		</div>
 	);
 }; 
